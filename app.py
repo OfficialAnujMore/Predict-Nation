9e68eccf3c5c6cc78a47b1200f7f5ae16e609eeb
moreanuj1307@gmail.com <moreanuj1307@gmail.com>
2021-04-08 17:30:34 +0530

    minor

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
-from flask import Flask, render_template,request,url_for
-from flask_bootstrap import Bootstrap 
+from flask import Flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap
 
 
 # NLP Packages
-from textblob import TextBlob,Word 
-import random 
+from textblob import TextBlob, Word
+import random
 import time
 
 app = Flask(__name__, template_folder='pages')
@@ -14,59 +14,57 @@ Bootstrap(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/login')
 def login():
-	return render_template('login.html')
-
+    return render_template('login.html')
 
 
 @app.route('/form')
 def form():
-	blob_polarity = 0 
-	blob_subjectivity = 0
-	sentiment = 'None'
-	received_text = 'None'
-	return render_template('input_form.html',
-							blob_polarity = blob_polarity,
-							blob_subjectivity = blob_subjectivity,
-							sentiment = sentiment,
-							)
+    blob_polarity = 0
+    blob_subjectivity = 0
+    sentiment = 'None'
+    received_text = 'None'
+    return render_template('input_form.html',
+                           blob_polarity=blob_polarity,
+                           blob_subjectivity=blob_subjectivity,
+                           sentiment=sentiment,
+                           )
 
 
-@app.route('/analyse',methods = ['POST'])
+@app.route('/analyse', methods=['POST'])
 def analyse():
 
-	blob_polarity = 0 
-	blob_subjectivity = 0
-	sentiment = 'None'
-	received_text = 'None'
-	if request.method == 'POST':
-		raw_text = request.form['rawtext']
-		#NLP stuff
-		
-		blob = TextBlob(raw_text)
-		received_text2 = blob
-		blob_polarity = blob.sentiment.polarity
-		blob_subjectivity = blob.sentiment.subjectivity
-		
-		if blob_polarity < 0:
-			sentiment = 'Negative'
-		elif blob_polarity > 0:
-			sentiment = 'Positive'
-		else:
-			sentiment = "Neutral"
+    blob_polarity = 0
+    blob_subjectivity = 0
+    sentiment = 'None'
+    received_text = 'None'
+    if request.method == 'POST':
+        raw_text = request.form['rawtext']
+        # NLP stuff
 
-	return render_template('input_form.html', 
-							received_text = received_text2,
-							blob_polarity = round(blob_polarity*100,2),
-							blob_subjectivity = round(blob_subjectivity*100,2),
-							sentiment = sentiment,
-							)
+        blob = TextBlob(raw_text)
+        received_text2 = blob
+        blob_polarity = blob.sentiment.polarity
+        blob_subjectivity = blob.sentiment.subjectivity
 
+        if blob_polarity < 0:
+            sentiment = 'Negative'
+        elif blob_polarity > 0:
+            sentiment = 'Positive'
+        else:
+            sentiment = "Neutral"
 
+    return render_template('input_form.html',
+                           received_text=received_text2,
+                           blob_polarity=round(blob_polarity*100, 2),
+                           blob_subjectivity=round(blob_subjectivity*100, 2),
+                           sentiment=sentiment,
+                           )
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
